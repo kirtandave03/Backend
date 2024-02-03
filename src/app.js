7b178fc0156c0a136adadbdaf57d32e25cdb5e7e
kirtandave03 <kirtan.dave@bosctechlabs.com>
2024-02-03 22:46:23 +0530

    util code

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
+const app = express(); // express() method na through badhi properties app naam na variable ma transfer thase.
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN //khali apda frontend ne j allow karisu backend sathe interact karva mate
+
+}))   // app.use method is used for middleware and configuration 
+
+app.use(express.json({limit: '16kb'})); //json ne accept karisu 
+app.use(express.urlencoded({limit: '16kb'})) //url mathi aavto encoded data mate nu configuration
+app.use(express.static('public'));
+app.use(cookieParser()); // server thi user na browser ni cookies pr CRUD operation karva
+
+
+
+module.exports = app;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 require('dotenv').config();
+const app = require('./app');
 // const mongoose = require('mongoose');
 // const DB_NAME = require('./constants.js')
 const connectDB = require('./db/db');
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{console.log(`Server is running at port ${process.env.PORT}`)})
+})
+.catch((err)=>{console.log("MongoDB connection failed !!!",err)});
 
 
 // (async ()=>{
